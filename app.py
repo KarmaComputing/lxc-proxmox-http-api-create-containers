7034cb7e0f4750740db522519dd8e5c604099099
chrisjsimpson <chris@karmacomputing.co.uk>
2021-04-05 17:54:05 +0100

    refactor new_container

--- app.py
+++ app.py
@@ -19,23 +19,25 @@ def create_app():
     try:
       container = Container(**request.get_json(force=True))
       container.status = 'creating'
-      command = f"pct create {container.id} --start --hostname {container.hostname} --net0 name={container.network.name},bridge={container.network.bridge},ip6={container.network.ipv6},gw6={container.network.gw6} --memory {container.memory} {container.template}"
-      subprocess.run(command, shell=True)
-      return f"Container created, probably. With container_id: {container_id}"
+      new_container(container)
+      return f"Container created, probably. With container id: {container.id}"
 
     except ValidationError as e:
       return e.json()
     return container.json()
 
+  @app.route('/health')
+  def health():
+    return "OK"
+
   @app.route('/')
   def create_random_container():
-      container_id = randrange(1000,9000)
-      command = f"pct create {container_id} --start --hostname {container_id} --net0 name=eth0,bridge=vmbr0,ip6=dhcp,gw6=2a01:4f8:160:2333:0:1:0:2 --memory 10240 local:vztmpl/debian-10-standard_10.7-1_amd64.tar.gz"
-
-      subprocess.run(command, shell=True)
-      return f"Container created, probably. With container_id: {container_id}"
+      id = randrange(1000,9000)
+      new_container(Container({"id": id, "hostname": id, "memory": 512, "status": "creating"}))
+      return f"Container created, probably. With container id: {id}"
 
   return app
+
 
 
 class Network(BaseModel):
@@ -52,7 +54,12 @@ class Container(BaseModel):
     network: Network = None
     template: Optional[str] = 'local:vztmpl/debian-10-standard_10.7-1_amd64.tar.gz'
     status: Optional[str] = ''
+
  
+def new_container(container: Container):
+      command = f"pct create {container.id} --start --hostname {container.hostname} --net0 name={container.network.name},bridge={container.network.bridge},ip6={container.network.ipv6},gw6={container.network.gw6} --memory {container.memory} {container.template}"
+      subprocess.run(command, shell=True)
+
 
 """
 Example:
